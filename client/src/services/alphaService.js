cd00f7a2607b27a8b219c98368e2274487e4ccba
josh <jboneplus7t@gmail.com>
2026-02-15 20:14:51 +0530

    final version

--- client/src/services/alphaService.js
+++ client/src/services/alphaService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/alpha"
+    baseURL: "https://alphabet-store-backend.onrender.com/alpha"
 })
 
 export const buyAlphas = async (ids, name) => {
